325dbe35407ee497a2c6fe367096b89c1a6ba93c
VoidCat <ZWj1154142014@hotmail.com>
2026-06-08 17:45:52 +0800

    Change site title from 'Ukncat' to 'VoidCat'

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-  title: "Ukncat",
+  title: "VoidCat",
   subtitle: "Blog",
   lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
   themeColor: {
